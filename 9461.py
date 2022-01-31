a2977404cc931c9f14391d5e1ff96403b70d6a0e
jiho Ha <hjh04311@gmail.com>
2022-01-31 20:43:55 +0900

    Create 9461.py

---
+++ 9461.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+  
+  tri = [1,1,1]
+  n = int(input())
+  for i in range(2,n):
+    tri.append(tri[i-1]+tri[i-2])
+    ## 점화식 : tri[i] = tri[i-3] + tri[i-2]
+  print(tri[n-1])
+
