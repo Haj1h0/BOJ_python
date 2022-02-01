3330d5ff478065097e76624b9fcb72d542f5590b
jiho Ha <hjh04311@gmail.com>
2022-02-01 22:21:29 +0900

    Update 4150.1_fibonacci.py

--- 4150.1_fibonacci.py
+++ 4150.1_fibonacci.py
@@ -1,18 +1,25 @@
 import sys 
 input = sys.stdin.readline
+
+# 피보나치 함수(Fibonacci Function)를 재귀함수로 구현 (top-down 다이나믹 프로그래밍)
+
+# 최대 재귀 깊이 변경을 위한 코드, 해당 코드 없을 시 Recursion Error 발생
+# 재귀 깊이가 너무 깊을 시 반복문을 활용한 bottom-up 다이나믹 프로그래밍으로 대체 사용 가능
 sys.setrecursionlimit(10**6)
 
-N = int(input())
-d = [0] * (N+1)
+n = int(input())
+# 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화, index 1부터 사용
+d = [0] * (n+1)
 
-def fibo(N):
-  if N == 1 or N == 2:
+def fibo(n):
+  # 종료 조건(1 혹은 2일 때 1을 반환)
+  if n == 1 or n == 2:
     return 1
-  if d[N] != 0:
-    return d[N]
-    # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
-  d[N] = fibo(N - 1) + fibo(N - 2)
-  #return fibo(N-1) + fibo(N-2)
-  return d[N]
+  # 이미 계산한 적 있는 문제라면 그대로 반환
+  if d[n] != 0:
+    return d[n]
+  # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
+  d[n] = fibo(n - 1) + fibo(n - 2)
+  return d[n]
 
-print(fibo(N))
+print(fibo(n))
