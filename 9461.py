cd4da08befa6166488fb5ae0a289d76a9106827f
jiho Ha <hjh04311@gmail.com>
2022-02-01 21:31:48 +0900

    Update 9461.py

--- 9461.py
+++ 9461.py
@@ -4,6 +4,6 @@ for _ in range(int(input())):
   n = int(input())
   for i in range(2,n):
     tri.append(tri[i-1]+tri[i-2])
-    ## 점화식 : tri[i] = tri[i-3] + tri[i-2]
+    # 점화식 : tri[i] = tri[i-3] + tri[i-2]
   print(tri[n-1])
 
