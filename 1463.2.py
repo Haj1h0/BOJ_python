260e65a035be83b5460879397b570d4b5f7a7799
jiho Ha <hjh04311@gmail.com>
2022-02-12 21:37:53 +0900

    Create 1463.2.py

---
+++ 1463.2.py
@@ -0,0 +1,16 @@
+import sys 
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0 for _ in range(n+1)]  # dp table
+
+for i in range(2, n+1): 
+    dp[i] = dp[i-1] + 1  
+    if i%2 == 0 and dp[i] > dp[i//2] + 1 :
+        dp[i] = dp[i//2]+1
+    if i%3 == 0 and dp[i] > dp[i//3] + 1 :
+        dp[i] = dp[i//3] + 1
+        
+print(dp[n])
+
+# 점화식 : dp(n) = min(dp(n//3)+1, dp(n//2)+1 , dp(n-1)+1 )
